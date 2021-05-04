66e2a6222f3f50da86d7cb1911d128a24d91d881
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-04 16:28:17 -0700

    finished back end

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,7 +12,7 @@ const typeDefs = gql`
         thoughts: [Thought]
         friends: [User]
     }
-    
+
     type Thought {
         _id: ID
         thoughtText: String
@@ -30,10 +30,24 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
     }
     `;
 
--- server/server.js
+++ server/server.js
@@ -2,17 +2,24 @@ const express = require('express');
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+// import authMiddleware
+const { authMiddleware } = require('./utils/auth');
+// db in the connection folder
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
 //create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 //integrate our Apollo server with the Express application as middleware
