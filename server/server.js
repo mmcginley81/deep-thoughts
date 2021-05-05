4da469680904ab1c01ab1c00e081ae0204fffa5a
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-05 14:12:15 -0700

    combined front end to back end

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express');
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -27,6 +29,16 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build/index.html')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
