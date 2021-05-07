d4d799a2a030cbbfe25d9deb37803f20dd053fb7
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-05-07 14:06:52 -0700

    added reaction component

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="signup" component={Signup} />
+              <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
               <Route exact path="/thought/:id" component={SingleThought} />
 
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -4,6 +4,8 @@ import { useQuery } from '@apollo/react-hooks';
 import { useParams } from 'react-router-dom';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -33,6 +35,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
